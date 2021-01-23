224e1050c8c2c95a671d523d01250597a111efa3
ewertoncmiranda <sonardevplus@gmail.com>
2021-01-23 12:08:35 -0300

    to prod

--- src/main/java/hack/api/com/dto/VacinaDTO.java
+++ src/main/java/hack/api/com/dto/VacinaDTO.java
@@ -7,11 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Data
-@Getter
-@Setter
-@NoArgsConstructor
-@EqualsAndHashCode
+
 public class VacinaDTO {
 	
 	private Long id;
