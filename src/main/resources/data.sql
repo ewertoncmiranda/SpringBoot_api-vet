d493557fc20c4fcccf43f3dceb95edeb59a85066
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2021-04-22 20:20:26 -0300

    ARQUIVO DATA.SQL ADICIONADO

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO vacina_tbl (id , nome , lote) VALUES ('1','RAIVA','2021') ;
+INSERT INTO vacina_tbl (id , nome , lote) VALUES ('2','TETANICA','2018') ;
+INSERT INTO vacina_tbl (id , nome , lote) VALUES ('3','CASTRANTE','1999') ;
+
+INSERT INTO animal_tbl (id,nome,raca,peso) VALUES ('1','Dogao' ,'SRD','60');
+INSERT INTO animal_tbl (id,nome,raca,peso) VALUES ('2','Tocaio' ,'SRD','35');
+INSERT INTO animal_tbl (id,nome,raca,peso) VALUES ('3','Mium' ,'SRD','15');
+
+INSERT INTO  dono_tbl (id ,nome,CPF ,idade) VALUES ('1' ,'Riacho' ,'13319881795','25');
+INSERT INTO  dono_tbl (id ,nome,CPF ,idade) VALUES ('2' ,'Locote' ,'23454300442','43');
+INSERT INTO  dono_tbl (id ,nome,CPF ,idade) VALUES ('3' ,'Coxinha' ,'52523299031','60');
