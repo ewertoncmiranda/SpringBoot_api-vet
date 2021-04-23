4e475250d5f28d7069def75159d5209d0f6d5235
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2021-04-23 14:58:45 -0300

    Spring SECURITY e JWT implementados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,10 @@ INSERT INTO animal_tbl (id,nome,raca,peso) VALUES ('3','Mium' ,'SRD','15');
 INSERT INTO  dono_tbl (id ,nome,CPF ,idade) VALUES ('1' ,'Riacho' ,'13319881795','25');
 INSERT INTO  dono_tbl (id ,nome,CPF ,idade) VALUES ('2' ,'Locote' ,'23454300442','43');
 INSERT INTO  dono_tbl (id ,nome,CPF ,idade) VALUES ('3' ,'Coxinha' ,'52523299031','60');
+
+INSERT INTO user_tbl(id ,nome ,senha) VALUES ('1' ,'admin','$2a$10$KR4XuZ8H2jrKaGIbnrFJdu26HITV1UDS05bDsE1COzr6/dHbCDyPK');
+INSERT INTO user_tbl(id ,nome ,senha) VALUES ('2' ,'user','$2a$10$RV9Z9SYRXyKIvmmil9Vf1u9Ni3KMmIiZQ6fp/YUyxPjajBjlOET.y');
+INSERT INTO user_tbl(id ,nome ,senha) VALUES ('3' ,'123',' ');
+
+INSERT INTO perfil_tbl(id,nome) VALUES ('1','ADMIN');
+INSERT INTO perfil_tbl(id,nome) VALUES ('2','NORMIE')
